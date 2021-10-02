fb47214e0c3e8d3db4556204984605952f3835d2
Gabriel Borges <bgfborges@gmail.com>
2021-10-02 17:39:26 -0300

    commit day 02/10

--- public/wp-content/themes/child-hello-elementor/woocommerce/lu-projects/lu_orders_associate_product.php
+++ public/wp-content/themes/child-hello-elementor/woocommerce/lu-projects/lu_orders_associate_product.php
@@ -1,5 +1,115 @@
 <?php
 
+
+add_action("gform_advancedpostcreation_post_after_creation", "create_donation_content", 10, 4);
+function create_donation_content($post_id, $feed, $entry, $form ){
+	//getting post id
+
+	// Checkboxes field id.
+    $field_id = 12;
+
+    // Get field object.
+    $field = GFAPI::get_field( $form, $field_id );
+
+    if ( $field->type == 'checkbox' ) {
+        // Get a comma separated list of checkboxes checked
+        $checked = $field->get_value_export( $entry );
+
+        // Convert to array.
+        // $values = explode( ', ', $checked );
+        $values = implode("|",$checked);
+
+		// Replace my_custom_field_key with your custom field meta key.
+		update_post_meta( $post_id, 'lu_base-price-defined-in-the-date', $values );
+    }
+
+	return;
+
+	if( $entry['12.1'] ){
+		update_post_meta($post_id, 'lu_landing-page-standard', 'on');
+	}
+	if( $entry['12.2'] ){
+		update_post_meta($post_id, 'lu_institutional-website-standard', 'on');
+	}
+	if( $entry['12.3'] ){
+		update_post_meta($post_id, 'lu_ecommerce-standard', 'on');
+	}
+
+	$comission_amount = 1000;
+	if( !$comission_amount || $comission_amount < 0 ) return;
+
+	$choosen_products = array(
+		'landing' => array(
+			'active' 	=> 'on',
+			'product_id' => 218,
+		),
+		'institutional' => array(
+			'active' 	=> 'on',
+			'product_id' => 3621,
+		),
+		'ecommerce' => array(
+			'active' 	=> 'on',
+			'product_id' => 3623,
+		),
+	);
+
+	foreach( $choosen_products as $key => $val ){
+		if( $val['active'] !== 'on' ){
+			unset( $choosen_products[$key] );
+		}
+	}
+
+	if( !$choosen_products ) return;
+
+	$loop = new WP_Query( array(
+		'post_type'         => 'shop_order',
+		'post_status'       =>  array_keys( wc_get_order_statuses() ),
+		'posts_per_page'    => -1,
+		'meta_query'	=> array(
+			array(
+				'key'       => 'lu_lu-id',
+				'value'     => strval($post_id),
+			),
+		),
+	) );
+
+	// The Wordpress post loop
+	if ( $loop->have_posts() ) return;
+
+	// Author is the person who created the order, then the client id to be associated
+	$author_id = get_post_field('post_author', $post_id);
+
+	$args = array(
+		'status' => 'pending',
+		'customer_id' => $author_id,
+	);
+
+	$new_order = wc_create_order($args);
+
+	foreach( $choosen_products as $key ){
+
+		$product = wc_get_product($key['product_id']);
+
+		$new_order->add_product($product, 1, [
+			'subtotal'     => $product->get_price(),
+			'total'        => $product->get_price(),
+		]);
+	}
+
+	$comission = wc_get_product(4612);
+
+	$new_order->add_product($comission, 1, [
+		'subtotal'     => $comission->get_price(),
+		'total'        => $comission->get_price() + $comission_amount,
+	]);
+
+	$new_order->calculate_totals();
+	$new_order->save();
+	update_post_meta( $new_order->get_id(), 'lu_lu-id', $post_id );
+
+}
+
+
 add_action( 'save_post_lu_orders', 'generate_wooorder_from_luorder' );
 function generate_wooorder_from_luorder($post_id) {
 
@@ -45,7 +155,7 @@ function generate_wooorder_from_luorder($post_id) {
 	if ( $loop->have_posts() ) return;
 
 	// Author is the person who created the order, then the client id to be associated
-	$author_id = get_post_field ('post_author', $post_id);
+	$author_id = get_post_field('post_author', $post_id);
 
 	$args = array(
 		'status' => 'pending',
